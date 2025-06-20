a4c1921f921c641b3da337f2184201e405bd973f
megha2475 <meghaks2003@gmail.com>
2025-06-20 16:42:59 +0530

    Add solution : sorted array in ascending order

---
+++ Arrays/ascending-order.c
@@ -0,0 +1,40 @@
+//Program to sort an array in ascending order
+
+#include <stdio.h>
+int main(){
+    int i,n,a[100],temp,j;
+    printf("Enter a limit:");
+    scanf("%d",&n);
+    printf("Enter the values:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
